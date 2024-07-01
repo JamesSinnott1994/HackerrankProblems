43742ad2aeb20b5402d9e2f0316500aabdd7e593
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2024-07-01 17:13:15 +0100

    [REFACTOR] Hashmap Two String problem - Optimized with Hashset and removal of unnecessary for loop

--- src/DictionariesAndHashmaps/TwoStrings.java
+++ src/DictionariesAndHashmaps/TwoStrings.java
@@ -1,7 +1,9 @@
 package DictionariesAndHashmaps;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class TwoStrings {
 
@@ -44,6 +46,27 @@ public class TwoStrings {
         // Check for substring
         for (Map.Entry<Character, Integer> entry : d1.entrySet()) {
             if (d2.containsKey(entry.getKey())) {
+                return "YES";
+            }
+        }
+
+        return "NO";
+
+    }
+
+    public static String twoStringsOptimized(String s1, String s2) {
+
+        // Use a HashSet to store characters from the first string
+        Set<Character> charactersInS1 = new HashSet<>();
+
+        // Add each character of s1 to the HashSet
+        for (Character c : s1.toCharArray()) {
+            charactersInS1.add(c);
+        }
+
+        // Check if any character in s2 is in the HashSet
+        for (Character c : s2.toCharArray()) {
+            if (charactersInS1.contains(c)) {
                 return "YES";
             }
         }
