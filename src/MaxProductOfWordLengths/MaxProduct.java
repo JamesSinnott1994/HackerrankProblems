026a22eaf138170da97f83721a0cbc3353fd0d2c
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2024-08-23 20:13:07 +0100

    [ADD] MaxProduct Hackerrank problem start

---
+++ src/MaxProductOfWordLengths/MaxProduct.java
@@ -0,0 +1,29 @@
+package MaxProductOfWordLengths;
+
+public class MaxProduct {
+
+    public static void main(String[] args) {
+
+        String[] words = {"abcw", "baz", "foo", "bar", "xtfn", "abcdef"};
+        int result = Result.maxProduct(words);
+
+    }
+
+}
+
+class Result {
+
+    /*
+    * Complete the "maxProduct" function below.
+    * The function takes array of string as parameter
+    *
+    * The functin is expected to return an INTEGER.
+     */
+
+    public static int maxProduct(String[] words) {
+        int maxProduct = 0;
+
+        return maxProduct;
+    }
+
+}
