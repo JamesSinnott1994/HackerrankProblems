8bc01579c97d97284a7ec6124d19ff8add38dddc
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2024-08-24 13:58:43 +0100

    [ADD] MaxProduct Hackerrank solution with optimization in character word check

--- src/MaxProductOfWordLengths/MaxProduct.java
+++ src/MaxProductOfWordLengths/MaxProduct.java
@@ -6,6 +6,7 @@ public class MaxProduct {
 
         String[] words = {"abcw", "baz", "foo", "bar", "xtfn", "abcdef"};
         int result = Result.maxProduct(words);
+        System.out.println("result : " + result);
 
     }
 
@@ -23,7 +24,54 @@ class Result {
     public static int maxProduct(String[] words) {
         int maxProduct = 0;
 
+        for (int i = 0; i < words.length; i++) {
+
+            for (int j = i+1; j < words.length; j++) {
+
+                int currentProduct = 0;
+
+                // Check that two words don't contain same letters
+                if (lettersDontMatch(words[i], words[j])) {
+                    currentProduct = words[i].length() * words[j].length();
+                }
+
+                if (currentProduct > maxProduct) {
+                    maxProduct = currentProduct;
+                }
+            }
+        }
         return maxProduct;
+    }
+
+    //firstWord.length() = 1000;  nextWord.length() = 1000
+    //firstWord.length() = 1000;  nextWord.length() = 1 ->1000
+    //firstWord.length() = 1;  nextWord.length() = 1000 ->1
+    public static boolean lettersDontMatch(String firstWord, String nextWord) {
+
+        /*
+        Check to see if the letters in both words are different and do not match
+         */
+
+        // We want to iterate over the smaller word as there will be less iterations. See comments above.
+        // OPTIMIZATION 1!
+        String wordToIterate;
+        String wordToCompare;
+        if (firstWord.length() <= nextWord.length()) {
+            wordToIterate = firstWord;
+            wordToCompare = nextWord;
+        } else {
+            wordToIterate = nextWord;
+            wordToCompare = firstWord;
+        }
+
+        for (Character c : wordToIterate.toCharArray()) {
+            if (wordToCompare.indexOf(c) != -1) { // Letter exists
+                return false;
+            } else { // Letter does not exist
+                continue;
+            }
+        }
+        return true;
     }
 
 }
