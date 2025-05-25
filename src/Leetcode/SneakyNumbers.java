fb568e7a2892e05b3c74c9817885751f97945d93
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2025-05-25 14:36:12 +0100

    [ADD] Find Sneaky Numbers - Optimized solution

--- src/Leetcode/SneakyNumbers.java
+++ src/Leetcode/SneakyNumbers.java
@@ -4,6 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SneakyNumbers {
+
+    /*
+    In the town of Digitville, there was a list of numbers called nums containing integers from 0 to n - 1.
+    Each number was supposed to appear exactly once in the list, however, two mischievous numbers sneaked in an
+    additional time, making the list longer than usual.
+
+As the town detective, your task is to find these two sneaky numbers. Return an array of size two containing the two
+numbers (in any order), so peace can return to Digitville.
+     */
+
     public int[] getSneakyNumbers(int[] nums) {
         // Iterate through
         // Add num to a map
@@ -24,6 +34,23 @@ public class SneakyNumbers {
                 numbersFound.put(num, true);
             }
         }
+        return results;
+    }
+
+    public static int[] getSneakyNumbersOptimized(int[] nums) {
+        // O(1)
+        int[] results = new int[2];
+        int idx = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = Math.abs(nums[i]); // Getting the num VALUE at each position
+            if (nums[num] < 0) {
+                results[idx++] = num;
+            } else {
+                nums[num] = -nums[num];
+            }
+        }
+
         return results;
     }
 }
