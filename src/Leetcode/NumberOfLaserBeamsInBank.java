0109a903237c8de1539a4742206fcdda5ed809da
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2025-05-26 18:05:23 +0100

    [ADD] Get NumberOfBeamsInBank (Med) - Optimized solution

--- src/Leetcode/NumberOfLaserBeamsInBank.java
+++ src/Leetcode/NumberOfLaserBeamsInBank.java
@@ -1,6 +1,26 @@
 package Leetcode;
 
 public class NumberOfLaserBeamsInBank {
+
+    public static int numberOfBeamsOptimized(String[] bank) {
+        int prev = 0, ans = 0;
+
+        for (String s: bank) {
+            int count = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '1') {
+                    count++;
+                }
+            }
+            if (count > 0) {
+                ans += prev * count;
+                prev = count;
+            }
+        }
+
+        return ans;
+    }
+
     public static int numberOfBeams(String[] bank) {
         // Variable countOfBeams
         // Iterate through each row
