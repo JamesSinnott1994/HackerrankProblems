62ab8bcf0aa03cb09af58cecde4b10ff6a5da796
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2025-11-30 11:53:18 +0000

    [ADD] MakeArrayDivisibleByK (Easy) - O(N) Remainder solution

--- src/Leetcode/MakeArrayDivisibleByK.java
+++ src/Leetcode/MakeArrayDivisibleByK.java
@@ -30,4 +30,14 @@ public class MakeArrayDivisibleByK {
         return Arrays.stream(nums).sum();
     }
 
+    public static int minOperationsBetterSolution(int[] nums, int k) {
+        // Just get remainder. That's how many operations you need
+
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++){
+            sum+=nums[i];
+        }
+        return sum%k;
+    }
+
 }
